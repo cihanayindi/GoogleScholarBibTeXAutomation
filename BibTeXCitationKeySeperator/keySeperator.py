ed9cd401b847bfce76e957f7ef1aa4e884dcb24a
Cihan Ayindi <cihan_ayindi@hotmail.com>
2025-03-03 02:01:16 +0300

    New Scripts are Added
    - A script was written that automatically extracts Citation Keys from a BibTeX data TXT file and assigns the keys to a new txt.
    
    - A script was written that automatically replaces the reference numbers specified in the article with the citation key of the relevant reference and gives the new article text.

---
+++ BibTeXCitationKeySeperator/keySeperator.py
@@ -0,0 +1,23 @@
+import re
+
+def keySeperator(input_file_path):
+    # Citation Key Seperator from Bibtex File
+    
+    # Read the input file
+    with open(input_file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    content = content.split("@")
+    content = content[1:]  # Skip the first element (it's empty)
+
+    keys = []
+
+    for bibtex in content:
+        match = re.search(r"{(.*?),", bibtex)  # Gets the first value in the first curly bracket
+        if match:
+            keys.append(match.group(1))  # Add to first object to keys list
+    with open("outputForKeys.txt", "w", encoding="utf-8") as output:
+        for key in keys:
+            output.write(f"{key}\n")
+
+keySeperator("inputBibTeX.txt")
---
+++ ReferencesModifierWithBibTeXKeys/modifier.py
@@ -0,0 +1,32 @@
+# Description: This script is used to modify the references in the text file.
+# The script reads the input file and the keys file, and then replaces the references in the input file with the corresponding keys from the keys file.
+import re
+
+def modifier(input_file_path, keys_path, output_path):
+    # Read to TXT and convert to list
+    with open(keys_path, "r", encoding="utf-8") as file:
+        references = [line.strip() for line in file.readlines()]
+
+    # Read to Input TXT and convert to string
+    with open(input_file_path, "r", encoding="utf-8") as file:
+        text = file.read()
+
+    # Find to references in string (example, (1, 2, 3, 4, 5, 6))
+    pattern = r"\(([\d,\s]+)\)" # pattern for find references
+
+    def replace_references(match):
+        numbers = match.group(1)  # "1, 2, 3 , 4, 5, 6"
+        ref_list = [int(num.strip()) - 1 for num in numbers.split(",")]  # Convert to integers to list
+        ref_names = [references[i] for i in ref_list]  # Take to reference names
+        # In the new format, add \cite{} and return
+        return r"\cite{" + ", ".join(ref_names) + "}"  # return in \cite 
+
+    # Change references in the text
+    updated_text = re.sub(pattern, replace_references, text)
+
+    # Write the updated text
+    with open(output_path, "w", encoding="utf-8") as file:
+        file.write(updated_text)
+    
+
+modifier("inputText.txt","outputForKeys.txt","outputText.txt")
